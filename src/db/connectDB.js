51d871b1cd3927672376f8c5b98ae33c9917b889
R4ms3s13 <95193718+R4ms3s13@users.noreply.github.com>
2022-10-10 12:15:06 -0400

    Update connectDB.js

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -3,7 +3,8 @@ const{ connect} = require("mongoose");
 async function connectDB(){
 
     try {
-        await connect('mongodb://localhost:27017/mit-crud-jwt',{
+        /// docker port
+        await connect('mongodb://localhost:27020/mit-crud-jwt',{
             useNewUrlParser: true,
             useUnifiedTopology: true
           })
